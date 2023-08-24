c0f2f03277e95137c1afcf1abc19afae8a3b6408
Erick <erick.kuwahara79@gmail.com>
2023-08-24 16:10:27 -0300

    add: initial files

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Porto Seguro Bike",
+  description: "Contrate o seguro para sua bicicleta!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex bg-white">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
