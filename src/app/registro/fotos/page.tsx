9d343ce6e96f831e5e661a8c16fd0678744ab4a6
Gustavo Costa <gustavocostamiec@gmail.com>
2023-09-06 10:23:02 -0300

    feat: 'fotos' page content

--- src/app/registro/fotos/page.tsx
+++ src/app/registro/fotos/page.tsx
@@ -2,10 +2,54 @@
 
  
 
+import { Banner } from "@/components/Banner";
+import { FileUpload } from "@/components/FileUpload";
+import { Actions } from "@/patterns/Actions";
+
+ 
+
 export default function Fotos() {
+  function handleBikePhotos() {}
+
+ 
+
   return (
 <>
-      Fotos
+<Banner
+        title="Envie as fotos da bike"
+        text="Tire ou anexe fotos da sua bike para nossa IA analisá-las."
+      />
+
+ 
+
+      <div className="flex flex-col gap-6">
+<div className="space-y-1">
+<h2 className="text-lg font-medium">Foto da dianteira</h2>
+<FileUpload id="file" />
+</div>
+<div className="space-y-1">
+<h2 className="text-lg font-medium">Foto da traseira</h2>
+<FileUpload id="file2" />
+</div>
+<div className="space-y-1">
+<h2 className="text-lg font-medium">Foto da lateral direita</h2>
+<FileUpload id="file3" />
+</div>
+<div className="space-y-1">
+<h2 className="text-lg font-medium">Foto da lateral esquerda</h2>
+<FileUpload id="file4" />
+</div>
+<div className="space-y-1">
+<h2 className="text-lg font-medium">Foto do número de série*</h2>
+<FileUpload id="file5" />
+</div>
+</div>
+
+ 
+
+      <div className="grid gap-4 sm:grid-cols-2">
+<Actions onStepCompletion={handleBikePhotos} />
+</div>
 </>
   );
 }
