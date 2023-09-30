bfc8dbe910313f491ecd2847dc6e56fbf63bcc08
Erick <erick.kuwahara79@gmail.com>
2023-09-30 14:59:09 -0300

    fix: font rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { RegisterProvider } from "@/contexts/RegisterContext";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Porto Seguro Bike",
