4d22960ceb74a35d978fbea026eb7faee83e118c
Erick <erick.kuwahara79@gmail.com>
2023-10-19 16:27:53 -0300

    fix: routing

--- src/app/registro/bike/detalhes/page.tsx
+++ src/app/registro/bike/detalhes/page.tsx
@@ -3,9 +3,14 @@
 import { Banner } from "@/components/layout/banner";
 import { Actions } from "@/components/layout/actions";
 import { Accessory } from "@/components/layout/section-accessory";
+import { useRouter } from "next/navigation";
 
 export default function Detalhes() {
-  function handleParts() {}
+  const router = useRouter();
+
+  function handleParts() {
+    router.push("/registro/bike");
+  }
 
   return (
     <>
